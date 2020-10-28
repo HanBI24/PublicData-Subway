dc6d53bf850c71f8a87f0b5f3ffb64c96d1b6183
HanBI24 <alsltjdrhs@naver.com>
2020-10-29 03:13:33 +0900

    Remove unnecessary code

--- app/src/main/java/hello/world/datagostation/MainActivity.java
+++ app/src/main/java/hello/world/datagostation/MainActivity.java
@@ -28,14 +28,11 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private EditText editText;
-    private TextView textView;
-    private XmlPullParser xpp;
     private ArrayList<String> stationNameArray;
     private ArrayList<String> stationLineArray;
     ListAdapter listAdapter;
     ListView listView;
     String key = "";
-    String data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,7 +40,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         editText = (EditText) findViewById(R.id.et);
-        textView = (TextView) findViewById(R.id.text);
         listView = (ListView) findViewById(R.id.list_item);
         stationNameArray = new ArrayList<>();
         stationLineArray = new ArrayList<>();
@@ -58,11 +54,9 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     public void searchStation(View view) {
         if (view.getId() == R.id.btn_station) {
-            // Change AsyncTask...
             new Thread(new Runnable() {
                 @RequiresApi(api = Build.VERSION_CODES.KITKAT)
                 @Override
@@ -78,10 +72,7 @@ public class MainActivity extends AppCompatActivity {
                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
-//                            textView.setText(data);
-
                             listAdapter.notifyDataSetChanged();
-//                            listAdapter.resetList();
                         }
                     });
                 }
@@ -96,33 +87,8 @@ public class MainActivity extends AppCompatActivity {
         listAdapter.resetList();
     }
 
-    public void searchAround(View view) {
-        if (view.getId() == R.id.btn_around) {
-            // Change AsyncTask...
-            new Thread(new Runnable() {
-                @RequiresApi(api = Build.VERSION_CODES.KITKAT)
-                @Override
-                public void run() {
-                    try {
-                        data = getXmlDataAround();
-                    } catch (UnsupportedEncodingException e) {
-                        e.printStackTrace();
-                    }
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            textView.setText(data);
-                        }
-                    });
-                }
-            }).start();
-        }
-    }
-
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     private void getXmlDataStation() throws UnsupportedEncodingException {
-//        StringBuilder buffer = new StringBuilder();
-
         String str = editText.getText().toString();
         String location = URLEncoder.encode(str, java.nio.charset.StandardCharsets.UTF_8.toString());
         String queryUrl = "http://openapi.tago.go.kr/openapi/service/SubwayInfoService/getKwrdFndSubwaySttnList?serviceKey=" + key + "&subwayStationName=" + location;
@@ -132,7 +98,7 @@ public class MainActivity extends AppCompatActivity {
             InputStream is = url.openStream();
 
             XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
-            xpp = factory.newPullParser();
+            XmlPullParser xpp = factory.newPullParser();
             xpp.setInput(new InputStreamReader(is, java.nio.charset.StandardCharsets.UTF_8.toString()));
 
             String tag;
@@ -143,25 +109,17 @@ public class MainActivity extends AppCompatActivity {
             while (eventType != XmlPullParser.END_DOCUMENT) {
                 switch (eventType) {
                     case XmlPullParser.START_DOCUMENT:
-//                        buffer.append("파싱 시작...\n\n");
                         break;
-
                     case XmlPullParser.START_TAG:
                         tag = xpp.getName();
                         if (tag.equals("item")) ;
                         else if (tag.equals("subwayRouteName")) {
-//                            buffer.append("Station Line: ");
                             xpp.next();
                             stationLineArray.add(xpp.getText());
-//                            buffer.append(xpp.getText());
-//                            buffer.append("\n");
                         } else if (tag.equals("subwayStationName")) {
-//                            buffer.append("Station Name: ");
                             xpp.next();
                             stationNameArray.add(xpp.getText());
                             Log.d("1234", xpp.getText());
-//                            buffer.append(xpp.getText());
-//                            buffer.append("\n");
                         }
                         break;
 
@@ -170,7 +128,6 @@ public class MainActivity extends AppCompatActivity {
 
                     case XmlPullParser.END_TAG:
                         tag = xpp.getName();
-//                        if (tag.equals("item")) buffer.append("\n");
                         break;
                 }
                 eventType = xpp.next();
@@ -178,72 +135,5 @@ public class MainActivity extends AppCompatActivity {
         } catch (Exception e) {
             e.printStackTrace();
         }
-//        buffer.append("Parsing END");
-//        return buffer.toString();
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
-    private String getXmlDataAround() throws UnsupportedEncodingException {
-        StringBuilder buffer = new StringBuilder();
-
-        String str = editText.getText().toString();
-        String location = URLEncoder.encode(str, java.nio.charset.StandardCharsets.UTF_8.toString());
-        String queryUrl = "http://openapi.tago.go.kr/openapi/service/SubwayInfoService/getKwrdFndSubwaySttnList?serviceKey=" + key + "&subwayStationName=" + location;
-
-        try {
-            URL url = new URL(queryUrl);
-            InputStream is = url.openStream();
-
-            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
-            xpp = factory.newPullParser();
-            xpp.setInput(new InputStreamReader(is, java.nio.charset.StandardCharsets.UTF_8.toString()));
-
-            String tag;
-
-            xpp.next();
-            int eventType = xpp.getEventType();
-
-            while (eventType != XmlPullParser.END_DOCUMENT) {
-                switch (eventType) {
-                    case XmlPullParser.START_DOCUMENT:
-                        buffer.append("파싱 시작...\n\n");
-                        break;
-
-                    case XmlPullParser.START_TAG:
-                        tag = xpp.getName();
-                        if (tag.equals("item")) ;
-                        else if (tag.equals("subwayRouteName")) {
-                            buffer.append("Station Line: ");
-                            xpp.next();
-                            buffer.append(xpp.getText());
-                            buffer.append("\n");
-                        } else if (tag.equals("subwayStationId")) {
-                            buffer.append("Station Code: ");
-                            xpp.next();
-                            buffer.append(xpp.getText());
-                            buffer.append("\n");
-                        } else if (tag.equals("subwayStationName")) {
-                            buffer.append("Station Name: ");
-                            xpp.next();
-                            buffer.append(xpp.getText());
-                            buffer.append("\n");
-                        }
-                        break;
-
-                    case XmlPullParser.TEXT:
-                        break;
-
-                    case XmlPullParser.END_TAG:
-                        tag = xpp.getName();
-                        if (tag.equals("item")) buffer.append("\n");
-                        break;
-                }
-                eventType = xpp.next();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        buffer.append("Parsing END");
-        return buffer.toString();
     }
 }
